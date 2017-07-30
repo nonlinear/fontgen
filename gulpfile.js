dd25cec85e217ed725e683ef46b337c0888dd398
Nicholas Frota <info@nicholasfrota.com>
2017-07-29 20:52:04 -0400

    cleaning gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 var gulp = require('gulp'),
-$ = require('gulp-load-plugins')(),
-requireDir = require('require-dir');
+fontgen = require('gulp-fontgen'),
+del = require('del'),
+runSequence = require('run-sequence');
 
-requireDir('./gulp/tasks');
+// moves dist to gh-pages
 
+gulp.task('fontgen1', function(callback){
+  del('./fonts', callback);
+});
 
+gulp.task('fontgen2', function() {
+  return gulp.src('./fontgen/*.{ttf,otf}')
+  .pipe(fontgen({
+    dest: './fonts/'
+  }));
+});
 
+gulp.task('fontgen', function(callback) {
+  runSequence(
+    ['fontgen1'],
+    ['fontgen2'],
+    callback
+    );
+});
